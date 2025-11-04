54c66d6aec36bd5026fb2797b69b074444c48ffa
Danilo Krausz <danilo.krausz@gmail.com>
2025-11-04 10:55:05 +0100

    First commit

---
+++ FrontEnd/src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const googleAuth = import.meta.env.VITE_GOOGLE_AUTH_ID;
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <GoogleOAuthProvider clientId={googleAuth}>
+      <App />
+    </GoogleOAuthProvider>
+  </StrictMode>
+);
